d6f642664be8d6ab5f9089eae9c569a629fc87fc
joselinecedano <joselinecedano0@gmail.com>
2023-07-20 14:58:22 -0400

    showing data on show page for each item on list

--- frontend/src/Pages/Show.js
+++ frontend/src/Pages/Show.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+import { useParams } from "react-router-dom";
 
-const Show = () => {
+const Show = (props) => {
+  const params = useParams();
+  const id = params.id;
+  const bones = props.skeleton;
+  const bone = bones.find((b) => b._id === id);
+
   return (
-    <div>Show</div>
-  )
-}
+    <div className="bone">
+      <h1>{bone.name}</h1>
+      <h3>{bone.age} years old</h3>
+      <h2>{bone.description}</h2>
+    </div>
+  );
+};
 
-export default Show
+export default Show;
