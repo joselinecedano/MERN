1de76eb0b8561d1bb775c7ce1d32a8a1c4991037
joselinecedano <joselinecedano0@gmail.com>
2023-07-20 13:38:31 -0400

    added components and pages folder w/ boilers

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import Header from './Components/Header';
+import Main from './Components/Main';
 
 function App() {
   return (
     <div className="App">
-     <h1> hello skeleton </h1>
+     {/* <h1> hello skeleton </h1> */}
+     <Header />
+     <Main />
     </div>
   );
 }
